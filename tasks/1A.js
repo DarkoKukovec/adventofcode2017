47bc6cc6dd1cebd3b9376b4bc0aa82e9405f3e5e
Darko Kukovec <darko@kukovec.eu>
2017-12-01 17:26:09 +0100

    Update code style

--- tasks/1A.js
+++ tasks/1A.js
@@ -4,9 +4,5 @@ console.log(
   loadData(1)
     .split('')
     .map(Number)
-    .reduce((sum, item, index, arr) => {
-      return (item === arr[(index + 1) % arr.length])
-        ? sum + item
-        : sum;
-    }, 0)
+    .reduce((sum, item, index, arr) => (item === arr[(index + 1) % arr.length]) ? sum + item : sum, 0)
 );

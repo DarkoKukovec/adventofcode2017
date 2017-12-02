ea4177d975c598489988daa053347a597a34442d
Darko Kukovec <darko@kukovec.eu>
2017-12-02 11:36:00 +0100

    Day 2 solutions

---
+++ tasks/2B.js
@@ -0,0 +1,15 @@
+const {loadData} = require('../helpers');
+
+console.log(
+  loadData(2)
+    .split('\n')
+    .map((row) => row
+      .split(/\s+/)
+      .map(Number)
+      .reduce(
+        (div, curr, index, arr) => Math.max(div, ...arr
+          .map((curr2, index2) => index2 !== index && curr % curr2 === 0 ? curr / curr2 : 0)
+      ), 0)
+    )
+    .reduce((sum, curr) => sum + curr)
+);

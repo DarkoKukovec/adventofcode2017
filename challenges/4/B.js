c243acd8413de0754120f3afa6371b10da955ca4
Darko Kukovec <darko@kukovec.eu>
2017-12-05 18:40:40 +0100

    Minimize

--- challenges/4/B.js
+++ challenges/4/B.js
@@ -1,9 +1,9 @@
 module.exports = (input) => input
-.split('\n')
-.filter((pass) =>  pass
-  .split(/\s+/)
-  .reduce((map, item) => (!map || map.includes(item.split('').sort().join('')))
-    ? false
-    : map.concat(item.split('').sort().join('')),
-  []))
-.length;
+  .split('\n')
+  .filter((pass) =>  pass
+    .split(/\s+/)
+    .reduce((map, item) => (!map || map.includes(item.split('').sort().join('')))
+      ? false
+      : map.concat(item.split('').sort().join('')),
+    []))
+  .length;

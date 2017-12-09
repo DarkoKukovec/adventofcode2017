7ca4de11ebef9028d41aac69de6989237c8027f8
Darko Kukovec <darko@kukovec.eu>
2017-12-09 12:00:17 +0100

    Tweaks

--- challenges/1/B.js
+++ challenges/1/B.js
@@ -1,4 +1,4 @@
 module.exports = (input) => input
   .split('')
   .map(Number)
-  .reduce((sum, item, index, arr) => (item === arr[(index + Math.round(arr.length / 2)) % arr.length]) ? sum + item : sum, 0);
+  .reduce((sum, item, index, arr) => (item === arr[(index + arr.length / 2) % arr.length]) ? sum + item : sum, 0);

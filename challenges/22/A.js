198bec4856afa33f33650fd0064e065520ff3cfe
Darko Kukovec <darko@kukovec.eu>
2017-12-23 14:29:54 +0100

    Tasks 22 optimizations

--- challenges/22/A.js
+++ challenges/22/A.js
@@ -1,27 +1,26 @@
 module.exports = (input) => {
   const rawMap = input.trim().split('\n').map((line, y) => line.split(''));
-  const map = [].concat(...rawMap
-    .map((line, y) => line
-      .reduce((infected, item, x) => item === '#' ? infected.concat([`${x},${y}`]) : infected, [])
-    ));
+  const infected = {}
+  rawMap.map((line, y) => line.map((item, x) => {
+    infected[`${x},${y}`] = item === '#';
+  }));
   const position = [Math.floor(rawMap[0].length / 2), Math.floor(rawMap.length / 2)];
   let dir = [0, -1];
-  let infected = 0;
+  let infectedCount = 0;
 
   for (let step = 0; step < 10000; step++) {
-    const coord = `${position[0]},${position[1]}`;
-    if (map.indexOf(coord) !== -1) {
+    const pos = position.join(',');
+    if (infected[pos]) {
       dir = [-dir[1], dir[0]];
-      const coordIndex = map.indexOf(coord);
-      map.splice(coordIndex, 1);
+      infected[pos] = false;
     } else {
       dir = [dir[1], -dir[0]];
-      infected++;
-      map.push(coord);
+      infectedCount++;
+      infected[pos] = true;
     }
     position[0] += dir[0];
     position[1] += dir[1];
   }
 
-  return infected;
+  return infectedCount;
 }
--- challenges/22/B.js
+++ challenges/22/B.js
@@ -1,48 +1,35 @@
-function check(position, map) {
-  return position[0] in map && map[position[0]][position[1]];
-}
-
-function move(position, from, to) {
-  if (from) {
-    from[position[0]][position[1]] = false;
-  }
-  if (to) {
-    to[position[0]] = to[position[0]] || [];
-    to[position[0]][position[1]] = true;
-  }
-}
-
 module.exports = (input) => {
   const rawMap = input.trim().split('\n').map((line, y) => line.split(''));
-  const infected = []
+  const map = {}
   rawMap.map((line, y) => line.map((item, x) => {
-    if (item === '#') {
-      infected[x] = infected[x] || [];
-      infected[x][y] = true;
-    }
+    map[`${x},${y}`] = item === '#' ? 2 : 0;
   }));
+
   const position = [Math.floor(rawMap[0].length / 2), Math.floor(rawMap.length / 2)];
-  let dir = [0, -1];
+  let dir = 0;
   let infections = 0;
-  const weakened = [];
-  const flagged = [];
+  const weakened = {};
+  const flagged = {};
+
+  const dirs = [
+    [0, -1],
+    [1, 0],
+    [0, 1],
+    [-1, 0],
+  ]
+
+  const dirChange = [3, 0, 1, 2];
 
   for (let step = 0; step < 10000000; step++) {
-    if (check(position, infected)) {
-      dir = [-dir[1], dir[0]];
-      move(position, infected, flagged);
-    } else if (check(position, weakened)) {
+    const pos = position[0] + ',' + position[1];
+    const state = map[pos] || 0;
+    if (state === 1) {
       infections++;
-      move(position, weakened, infected);
-    } else if (check(position, flagged)) {
-      dir = [-dir[0], -dir[1]];
-      move(position, flagged, null);
-    } else {
-      dir = [dir[1], -dir[0]];
-      move(position, null, weakened);
     }
-    position[0] += dir[0];
-    position[1] += dir[1];
+    dir = (dir + dirChange[state]) % 4;
+    map[pos] = (state + 1) % 4;
+    position[0] += dirs[dir][0];
+    position[1] += dirs[dir][1];
   }
 
   return infections;
